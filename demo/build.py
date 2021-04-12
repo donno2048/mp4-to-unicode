9a0faeb82dc75ebd67fc20d69b290daae633d2b8
Elisha Hollander <just4now666666@gmail.com>
2021-04-13 02:13:37 +0300

    Update build.py

--- demo/build.py
+++ demo/build.py
@@ -1,6 +1,6 @@
 from PyInstaller.__main__ import run
 from shutil import rmtree
 from os import unlink
-run(['main.py', '-y', '-F', '-c', '-i=logo.ico', '--add-data=video.mp4;.', '-n=main.exe'])
+run(['main.py', '-y', '-F', '--console', '-i=logo.ico', '--add-data=video.mp4;.', '-n=main.exe'])
 rmtree('build')
 unlink('main.exe.spec')
