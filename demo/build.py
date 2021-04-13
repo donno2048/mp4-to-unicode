469853f468f437297541df1c9d684e77ef2a7aea
donno2048 <just4now666666@gmail.com>
2021-04-13 03:19:23 +0300

    Revert "Update build.py"
    This reverts commit 3cfc06426c54a2442746203df1d3a75ee1cfe78b.

--- demo/build.py
+++ demo/build.py
@@ -1,7 +1,6 @@
 from PyInstaller.__main__ import run
 from shutil import rmtree
 from os import unlink
-try: run(['main.py', '-y', '-F', '--console', '-i=logo.ico', '--add-data=video.mp4;.', '-n=main.exe'])
-except: run(['main.py', '-y', '-F', '--console', '-i=logo.ico', '--add-data=video.mp4:.', '-n=main.exe'])
+run(['main.py', '-y', '-F', '--console', '-i=logo.ico', '--add-data=video.mp4;.', '-n=main.exe'])
 rmtree('build')
 unlink('main.exe.spec')
